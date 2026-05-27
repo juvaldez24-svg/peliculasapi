acf710f7310edcba942681f04eadc3b7aa79040c
Johan <ju.valdez24@info.uas.edu.mx>
2026-05-26 19:23:54 -0700

    corregido

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+/*const { Sequelize } = require('sequelize');
 
 const urlPostgres = process.env.DATABASE_URL;
 
@@ -21,3 +21,20 @@ const sequelize = urlPostgres
     });
 
 module.exports = sequelize;
+*/
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Crucial para que Render acepte la conexión segura
+    }
+  },
+  logging: false // Cambia a console.log si quieres ver las consultas SQL en la terminal
+});
+
+export default sequelize;
